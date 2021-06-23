95cdb7d09e74a2f17c82e9d5f3dade03200e48a4
lizzochek <littled685@gmail.com>
2021-06-23 14:34:51 +0300

    Adding and deleting users implemented

--- login.js
+++ login.js
@@ -14,13 +14,21 @@ const write = (s) => rl.output.write(s);
 let inputEmail;
 let inputPassword;
 
-const inputEmailQuestion = "Введіть пошту у форматі hello@ginputEmail.com";
+const inputEmailQuestion = "Введіть пошту у форматі hello@ginputEmail.com  ";
 const inputEmailError =
   "Будь ласка спробуйте ще раз і введіть пошту у правильному форматі";
-const inputPasswordQuestion = "Введіть пароль, використовуючи літери та цифри";
-const inputPasswordError = "Не вірний пароль";
+const inputPasswordQuestion =
+  "Введіть пароль, використовуючи літери та цифри  ";
+const inputPasswordError =
+  "пароль не вірний. Перевірте правильність вводу даних";
 const userError =
   "Такого користувача не знайдено.\nПеревірте правильність даних або зареєструйтеся";
+const userExists =
+  "Користувач з таким імейлом вже існує\nХочете видалити аккаунт та  завести новий? (так/ні)  ";
+const thanksForRegistration =
+  "Дякуємо за реєстрацію\nПри наступному запуску програми ви можете увійти\nта подивитись поточний курс біткоїна до гривні";
+const userDeleted = "Користувача успішно видалено";
+const tryToLogin = "Спробуйте увійти або зареєструватися знову :)";
 
 const findUser = (inputEmail, inputPassword) => {
   let user = db.find((user) => user.email === inputEmail);
@@ -28,16 +36,40 @@ const findUser = (inputEmail, inputPassword) => {
     console.log(userError);
     rl.close();
   } else {
+    if (user.password !== inputPassword) {
+      console.log(inputPasswordError);
+      rl.close();
+    }
   }
 };
 
 const addUser = (inputEmail, inputPassword) => {
-  db.push({
-    email: inputEmail,
-    password: inputPassword,
-  });
-  console.log(db);
+  let user = db.findIndex((user) => user.email === inputEmail);
+  if (user < 0) {
+    db.push({
+      email: inputEmail,
+      password: inputPassword,
+    });
+
+    fs.writeFileSync("./db.json", JSON.stringify(db));
+    console.log(thanksForRegistration);
+    rl.close();
+  } else {
+    rl.question(userExists, (answer) => {
+      if (answer === "так") deleteUser(user);
+      if (answer === "ні") {
+        console.log(tryToLogin);
+        rl.close();
+      }
+    });
+  }
+};
+
+const deleteUser = (userIndex) => {
+  delete db[userIndex];
   fs.writeFileSync("./db.json", JSON.stringify(db));
+  console.log(userDeleted);
+  rl.close();
 };
 
 const loginInput = (callback) => {
@@ -58,7 +90,7 @@ const loginInput = (callback) => {
   });
 };
 
-rl.question("Вже зареєстровані? ", (answer) => {
+rl.question("Вже зареєстровані? (так/ні) ", (answer) => {
   if (answer.toLowerCase() === "так") loginInput(findUser);
   if (answer.toLowerCase() === "ні") loginInput(addUser);
 });
